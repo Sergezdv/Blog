bd1d040571f2c51a795c9489df7137cc678d5df1
Sergezdv <snz985@gmail.com>
2012-08-23 18:55:38 +0300

    add categories

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,7 @@ class Post < ActiveRecord::Base
   validates :content, :presence => true
 
   belongs_to :user
+
+  has_many :category_posts, :dependent => :destroy
+  has_many :categories, :through => :category_posts
 end
